577251f70180f22071be04b76fa656c01503ee0f
joegasewicz <joegasewicz@gmail.com>
2024-11-09 21:38:42 +0000

    initial

--- web_app/app.py
+++ web_app/app.py
@@ -1,48 +1,7 @@
-from bobtail import (
-    BobTail,
-    AbstractRoute,
-    Request,
-    Response,
-    BaseOptions,
-)
-from bobtail_cors import BobtailCORS
-from bobtail_logger import BobtailLogger
-from bobtail_upload import BobtailUpload
-from bobtail_jinja2 import BobtailJinja2
+from api import create_app
 
 
-class Options(BaseOptions):
-    STATIC_DIR = "static"
+app = create_app()
 
-
-class Static:
-
-    def get(self, req: Request, res: Response) -> None:
-        res.set_static(req.path)
-
-
-class Home:
-
-    def get(self, req: Request, res: Response):
-        res.set_headers({
-            "Content-Type": "text/plain",
-        })
-        data = {
-            "title": "Welcome To Joe's Tech Blog",
-            "h1": "Welcome To Joe's Tech Blog",
-        }
-        res.jinja2.render(res, "routes/home.jinja2", data=data)
-
-
-routes = [
-    (Static(), "/static/*"),
-    (Home(), "/"),
-]
-
-app = BobTail(Options, routes=routes)
-
-# middleware
-app.use(BobtailCORS())
-app.use(BobtailLogger())
-app.use(BobtailUpload())
-app.use(BobtailJinja2(template_dir="templates"))
+if __name__ == "__main__":
+    pass
